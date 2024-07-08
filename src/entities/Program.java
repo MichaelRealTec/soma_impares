4812d5d59ff0f984c6a4b1f8742469a00223bbd7
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-08 18:52:05 -0300

    Primeiro commit

---
+++ src/entities/Program.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Soma_impares
+		/*
+		 Leia 2 valores inteiros X e Y (em qualquer ordem). A seguir, calcule e mostre a soma dos números
+impares entre eles.
+exemplo:
+Digite dois numeros:
+2
+9
+SOMA DOS IMPARES = 15
+		 */
+		
+		
+
+	}
+
+}
